c9daa5c3aa51e69e8f705832b79bab94eb33040f
Zifeng Wang <zifengw2@illinois.edu>
2025-10-01 00:27:16 -0500

    init study search task sft building

---
+++ data_pipeline/vllm_host.sh
@@ -0,0 +1,7 @@
+export CUDA_VISIBLE_DEVICES=0,1,2,3
+
+python3 -m vllm.entrypoints.openai.api_server \
+    --model DeepRetrieval/DeepRetrieval-PubMed-3B-Llama \
+    --port 8000 \
+    --max-model-len 2048 \
+    --tensor-parallel-size 4 
