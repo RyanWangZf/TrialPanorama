b7925582f8d22e633419200900b301307382fa3f
Zifeng Wang <zifengw2@illinois.edu>
2025-10-01 11:57:28 -0500

    update vllm host script

--- data_pipeline/vllm_host.sh
+++ data_pipeline/vllm_host.sh
@@ -1,7 +1,7 @@
 export CUDA_VISIBLE_DEVICES=0,1,2,3
 
-python3 -m vllm.entrypoints.openai.api_server \
+nohup python3 -m vllm.entrypoints.openai.api_server \
     --model DeepRetrieval/DeepRetrieval-PubMed-3B-Llama \
     --port 8000 \
     --max-model-len 2048 \
-    --tensor-parallel-size 4 
+    --tensor-parallel-size 4 > vllm_host.log 2>&1 &
